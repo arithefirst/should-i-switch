a53073907e953bb465ca0d0f02ec11d4623e05e2
April Hall <ari@arithefirst.com>
2025-06-08 20:49:16 -0400

    feat: Add site href

--- src/components/homeBox.tsx
+++ src/components/homeBox.tsx
@@ -1,3 +1,5 @@
+import { LogoStamp } from "./logoStamp";
+
 interface HomeBoxProps {
   children: React.ReactNode;
   className?: string;
@@ -6,6 +8,7 @@ interface HomeBoxProps {
 export function HomeBox({ children, className }: HomeBoxProps) {
   return (
     <main className={"w-screen min-h-screen p-4 flex items-center justify-center" + ' ' + className}>
+      <LogoStamp/>
       <div className="md:w-1/2 w-screen rounded-md p-4 bg-gray-200 dark:bg-neutral-900">
         {children}
       </div>
---
+++ src/components/logoStamp.tsx
@@ -0,0 +1,6 @@
+export function LogoStamp() {
+  return <div className="fixed bottom-4 left-4">
+    <p>Built by <a href="https://www.arithefirst.com/" className="underline text-blue-500 hover:text-blue-600 transition-colors">April Hall</a></p>
+    <p>Licensed under the <a href="https://www.gnu.org/licenses/gpl-3.0.en.html" className="underline text-blue-500 hover:text-blue-600 transition-colors">GNU GPL 3.0</a></p>
+  </div>
+}
